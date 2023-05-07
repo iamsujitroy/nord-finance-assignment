1fcafd2e5a9edc654e323e862373b4027b0cae4c
Sujit Roy <sujitroy78148@gmail.com>
2023-05-08 00:13:50 +0530

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import "./App.css";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<NotFound />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <Home />
     </div>
   );
 }
